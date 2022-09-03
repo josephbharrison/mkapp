28680d07a0f44ed89a11c56e322421ceac84cf68
Joe Harrison <josephbharrison@gmail.com>
2022-09-03 19:25:02 -0400

    fix(sample): issue in local test

--- sample/Icarus.sh
+++ sample/Icarus.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-[[ -z $ICARUS_HOST ]] && ICARUS_HOST=localhost
-[[ -z $ICARUS_PORT ]] && ICARUS_PORT=3300
+# Intialize and export vars
+[[ -z $ICARUS_HOST ]] && export ICARUS_HOST=localhost
+[[ -z $ICARUS_PORT ]] && export ICARUS_PORT=3300
 
-# Start Icarus locally if not found
-if [[ ! nc -z $ICARUS_HOST $ICARUS_PORT && $ICARUS_HOST == localhost ]]; then  
+# Start Icarus if local and not running
+if ! nc -z $ICARUS_HOST $ICARUS_PORT && $ICARUS_HOST == localhost; then
     [[ -z $ICARUS_HOME ]] && ICARUS_HOME=$CODE_HOME/icarus
     cd $ICARUS_HOME || exit 1
     nohup npm run dev &
-fi
 
-while [[ ! nc -z $ICARUS_HOST $ICARUS_PORT ]]
-do
-    sleep 0.1
-done
+    while [[ $(! nc -z $ICARUS_HOST $ICARUS_PORT) -gt 0 ]]
+    do
+        sleep 0.1
+    done
+fi
 
 open "http://${ICARUS_HOST}:${ICARUS_PORT}"
